2cae511aa031aabafc12cf8346529b3494859684
Carlos Lopez <celm1990@gmail.com>
2023-06-21 22:08:08 -0500

    [FIX] setear usuario y correo de git para pasar test

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -9,5 +9,7 @@ here=$(dirname $0)
 testvenv=/tmp/testvenv 
 
 virtualenv -p python3 $testvenv
+git config --global user.name ${OCA_GIT_USER_NAME}
+git config --global user.email ${OCA_GIT_USER_EMAIL}
 $testvenv/bin/pip install pytest
 $testvenv/bin/pytest --color=yes --ignore $here/data $here "$@"
